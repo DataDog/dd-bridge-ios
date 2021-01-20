dd4a05b41910c897b4b254b6482b4dbf00deab47
Xavier Gouchet <xavier.gouchet@gmail.com>
2021-01-20 13:44:35 +0100

    Update generated code from bridge version 0.1

--- DatadogSDKBridge/Classes/Interface/DdTrace.swift
+++ DatadogSDKBridge/Classes/Interface/DdTrace.swift
@@ -14,10 +14,10 @@ public protocol DdTrace {
     /**
        Start a span, and returns a unique identifier for the span.
      */
-    func startSpan(operation: NSString, timestamp: Int64, context: NSDictionary) -> NSString
+    func startSpan(operation: NSString, timestampMs: Int64, context: NSDictionary) -> NSString
 
     /**
        Finish a started span.
      */
-    func finishSpan(spanId: NSString, timestamp: Int64, context: NSDictionary)
+    func finishSpan(spanId: NSString, timestampMs: Int64, context: NSDictionary)
 }
